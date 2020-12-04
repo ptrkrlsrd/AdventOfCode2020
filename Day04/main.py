229768e4c71b4cf0647b08983676f597369b140b
Petter Karlsrud <petterkarlsrud@me.com>
2020-12-04 10:16:24 +0100

    Day04 - Changed main and Dockerfile

--- Day04/main.py
+++ Day04/main.py
@@ -1,34 +1,37 @@
 import shlex
+from typing import List
 from schema import Schema, And, Use, Optional, SchemaError
 
-def check(conf_schema, conf, debug=False):
+def check(schema: Schema, data, debug=False):
     try:
-        conf_schema.validate(conf)
+        schema.validate(data)
         return True
     except SchemaError as e:
         if debug:
             print(e)
         return False
 
-validation_schema = Schema({
-        'byr': And(Use(str)),
-        'iyr': And(Use(str)),
-        'eyr': And(Use(str)),
-        'hgt': And(Use(str)),
-        'hcl': And(Use(str)),
-        'ecl': And(Use(str)),
-        'pid': And(Use(str)),
-        Optional('cid'): And(Use(str))
-})
+if __name__ == "__main__":
+    validation_schema = Schema({
+            'byr': And(Use(str)),
+            'iyr': And(Use(str)),
+            'eyr': And(Use(str)),
+            'hgt': And(Use(str)),
+            'hcl': And(Use(str)),
+            'ecl': And(Use(str)),
+            'pid': And(Use(str)),
+            Optional('cid'): And(Use(str))
+    })
 
-valid = 0
+    valid_passports: int = 0
 
-with open("./data/input.txt") as f:
-    content = f.read()
-    passports = content.split("\n\n")
-    for line in passports:
-        passport = dict(token.split(':') for token in shlex.split(line))
-        if check(validation_schema, passport):
-            valid += 1
+    with open("./data/input.txt") as f:
+        content: str = f.read()
+        passports: List[str] = content.split("\n\n")
 
-print("{}".format(valid))
+        for line in passports:
+            passport = dict(token.split(':') for token in shlex.split(line))
+            if check(validation_schema, passport):
+                valid_passports += 1
+
+    print("{}".format(valid_passports))
